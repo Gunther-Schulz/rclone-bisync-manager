03d3360173775a0e8fd70679cd400788d14b8063
Gunther Schulz <dev@guntherschulz.de>
2024-08-14 06:24:58 +0200

    upd todo

--- src/sync.py
+++ src/sync.py
@@ -4,8 +4,6 @@ from datetime import datetime
 from config import config
 from utils import is_cpulimit_installed, check_local_rclone_test, check_remote_rclone_test, ensure_local_directory
 from logging_utils import log_message, log_error
-import json
-import fcntl
 from config import config, sync_state
 
 
